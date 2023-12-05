f914809a45548ad7105720512afb8fe74333fbe7
AliAkcan1 <ali406akcan@gmail.com>
2023-12-06 01:24:19 +0300

    Still working on.

--- src/components/LocationService.jsx
+++ src/components/LocationService.jsx
@@ -4,13 +4,13 @@ import axios from 'axios'
 const URL = "http://localhost:8080/location/countries?page=1&size=50"
 function LocationService(){
     const [results, setResults] = useState([]);
-    async function fetchLocations(){
-        const response = await axios.get(URL);
-        setResults(response.data);
-        console.log(results);
-    }
 
     useEffect(() => {
+        async function fetchLocations(){
+            const response = await axios.get(URL);
+            setResults(response);
+            console.log(results);
+        }
         fetchLocations();
     }, [])
     return(
