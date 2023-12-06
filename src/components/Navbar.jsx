b5834294478030c7f6d388544ec2c0ca89aa887b
AliAkcan1 <ali406akcan@gmail.com>
2023-12-06 20:54:17 +0300

    hamburger menu>dropdown menu, some css bugs fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,31 +1,53 @@
-import Hamburger from './Hamburger'
-import {useState} from 'react';
+import { useState } from 'react';
 
-export default function Navbar(){
+export default function Navbar() {
   const [open, setOpen] = useState(false);
-  const closeButton = <button className='nav-button-2' onClick={() => setOpen(!open)}>
-                        <i class="fa-solid fa-x"></i>
-                      </button>;
-  const hamburgerButton = <button className='nav-button-2' onClick={() => setOpen(!open)}>
-                            <i className="fa-solid fa-bars"></i>
-                          </button>;
-                      
+
   return (
     <div>
       <nav className='navbar'>
         <div className="nav-logo">
-            <span className="sue">sue</span><span className='casey' >casey.</span>
+          <span className="sue">sue</span><span className='casey' >casey.</span>
         </div>
         <div className="nav-buttons">
-            <button className='nav-button-1'>
-              <a href="/login">
-                Let's Talk  <i className="fa-solid fa-arrow-right"></i>
-              </a>
+          <div className='dropdown-button'>
+            <button className='nav-button-2' onClick={() => setOpen(!open)}>
+              <i className={open ? "fa-solid fa-x" : "fa-solid fa-bars"}></i>
             </button>
-            {open ? closeButton : hamburgerButton}
+            <div className='dropdown-menu' style={open ? {visibility: "visible"} : {visibility: "hidden"}}>
+              <div className="dropdown-content">
+                <ul>
+                  <li className='dropdown-li'>
+                    <a href="/login">
+                      Login
+                    </a>
+                  </li>
+                  <li className='dropdown-li'>
+                    <a href="/signup">
+                      Sign Up
+                    </a>
+                  </li>
+                  <li className='dropdown-li'>
+                    <a href="/">
+                      Contact Us
+                    </a>
+                  </li>
+                  <li className='dropdown-li'>
+                    <a href="/">
+                      Become a Freelancer
+                    </a>
+                  </li>
+                  <li className='dropdown-li'>
+                    <a href="/">
+                      Terms
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
         </div>
-        {open && <Hamburger />}
       </nav>
     </div>
   )
-};
+}
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,7 +1,7 @@
 import LocationService from "../components/LocationService"
 
-export default function SignUp(){
-    return(
+export default function SignUp() {
+    return (
         <div className="signup-page">
             <LocationService />
             <div className="signup-main">
@@ -15,44 +15,46 @@ export default function SignUp(){
                             <div className="name-surname">
                                 <div className='d-signup-name signup-textbox-cnt' >
                                     <label htmlFor="signup-name" className='signup-label'>Name</label>
-                                    <input type="text" placeholder="Name" id='signup-name' className="signup-textbox signup-name"  required/>
+                                    <input type="text" placeholder="Name" id='signup-name' className="signup-textbox signup-name" required />
                                 </div>
                                 <div className="d-signup-surname signup-textbox-cnt">
                                     <label htmlFor="signup-surname" className='signup-label'>Last Name</label>
-                                    <input type="text" placeholder="Surname" id='signup-surname' className="signup-textbox signup-surname"  required/>
+                                    <input type="text" placeholder="Surname" id='signup-surname' className="signup-textbox signup-surname" required />
                                 </div>
                             </div>
                             <div className="d-signup-username signup-textbox-cnt">
                                 <label htmlFor="signup-username" className='signup-label'>Username</label>
-                                <input type="text" placeholder="Username" id='signup-username' className="signup-textbox signup-username"  required/>
+                                <input type="text" placeholder="Username" id='signup-username' className="signup-textbox signup-username" required />
                             </div>
                             <div className="d-signup-email signup-textbox-cnt">
                                 <label htmlFor="signup-email" className='signup-label'>E-Mail Adress</label>
-                                <input type="text" placeholder="E-Mail" id='signup-email' className="signup-textbox signup-email"  required/>
+                                <input type="text" placeholder="E-Mail" id='signup-email' className="signup-textbox signup-email" required />
                             </div>
                             <div className="location">
                                 <div className="d-signup-city signup-textbox-cnt">
                                     <label htmlFor="signup-city" className='signup-label'>City</label>
-                                    <input type="text" placeholder="City" id='signup-city' className="signup-textbox signup-city"  required/>
+                                    <input type="text" placeholder="City" id='signup-city' className="signup-textbox signup-city" required />
                                 </div>
                                 <div className="d-signup-country signup-textbox-cnt">
                                     <label htmlFor="signup-country" className='signup-label'>Country</label>
-                                    <input type="text" placeholder="Country" id='signup-country' className="signup-textbox signup-country"  required/>
+                                    <input type="text" placeholder="Country" id='signup-country' className="signup-textbox signup-country" required />
                                 </div>
                             </div>
                             <div className="d-signup-password signup-textbox-cnt">
                                 <label htmlFor="signup-surname" className='signup-label'>Password</label>
-                                <input type="password" placeholder="Password" id='signup-password' className="signup-textbox signup-password"  required/>
+                                <input type="password" placeholder="Password" id='signup-password' className="signup-textbox signup-password" required />
                             </div>
                             <div className="d-signup-verify signup-textbox-cnt">
                                 <label htmlFor="signup-verify" className='signup-label'>Verify Password</label>
-                                <input type="password" placeholder="Verify Password" id='signup-verify' className="signup-textbox signup-verify"  required/>
+                                <input type="password" placeholder="Verify Password" id='signup-verify' className="signup-textbox signup-verify" required />
                             </div>
                             <div className="d-signup-checkbox">
                                 <input type="checkbox" name="checkbox" id="signup-checkbox" value="accept" />
-                                <label htmlFor="signup-checkbox">I agree with suecasey.'s <a href='/'>Terms of Service</a>, Privacy Policy, and default Notification Settings.</label>
+                                <label htmlFor="signup-checkbox">I agree with suecasey&apos;s <a href='/'>Terms of Service</a>, Privacy Policy, and default Notification Settings.</label>
                             </div>
-                            <button className="signup-button">Sign Up</button>
+                            <div className="d-signup-submit">
+                                <button className="signup-button">Sign Up</button>
+                            </div>
                         </form>
                     </div>
                 </div>
