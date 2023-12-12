d89effa0a2c36ea0a82e7647bd801434b196a816
AliAkcan1 <ali406akcan@gmail.com>
2023-12-13 01:37:14 +0300

    location service in progress

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,9 +1,25 @@
-import LocationService from "../components/LocationService"
+import {useEffect, useState} from 'react'
+import axios from 'axios'
+
+const URL = "http://localhost:8080/location/countries?page=1&size=50"
 
 export default function SignUp() {
+    const [results, setResults] = useState([]);
+    useEffect(() => {
+         async function fetchLocations(){
+             const response = await axios.get(URL);
+             setResults(response);
+             console.log("Got the location list data.")
+         }
+         fetchLocations();
+    }, []);
+
+    function searchCountryList(){
+
+    }
+
     return (
         <div className="signup-page">
-            <LocationService />
             <div className="signup-main">
                 <div className="signup-main-header">
                     <h1>Create a new account.</h1>
@@ -29,16 +45,28 @@ export default function SignUp() {
                                 <label htmlFor="signup-email" className='signup-label'>E-Mail Adress</label>
                                 <input type="text" placeholder="E-Mail" id='signup-email' className="signup-textbox signup-email" required />
                             </div>
+
+                            {/* LOCATION SERVICE WORKS IN THIS AREA */}
                             <div className="location">
+                                <div className="d-signup-country signup-textbox-cnt">
+                                    <label htmlFor="signup-country" className='signup-label'>Country</label>
+                                    <input type="text" placeholder="Country" id='signup-country' className="signup-textbox signup-country" required />
+                                    <div className="country-search-list">
+                                        <ul>
+                                            <li>aa</li>
+                                            <li>aaaaa</li>
+                                            <li>aaaa</li>
+                                            <li>aaaaaa</li>
+                                            <li>aaaaaaa</li>
+                                        </ul>
+                                    </div>
+                                </div>
                                 <div className="d-signup-city signup-textbox-cnt">
                                     <label htmlFor="signup-city" className='signup-label'>City</label>
                                     <input type="text" placeholder="City" id='signup-city' className="signup-textbox signup-city" required />
                                 </div>
-                                <div className="d-signup-country signup-textbox-cnt">
-                                    <label htmlFor="signup-country" className='signup-label'>Country</label>
-                                    <input type="text" placeholder="Country" id='signup-country' className="signup-textbox signup-country" required />
-                                </div>
                             </div>
+                            {/* location service area */}
                             <div className="d-signup-password signup-textbox-cnt">
                                 <label htmlFor="signup-surname" className='signup-label'>Password</label>
                                 <input type="password" placeholder="Password" id='signup-password' className="signup-textbox signup-password" required />
